f21b06e0999e91c97fae9c7e844d14f0cef6cec8
Syntaf <gmercer015@gmail.com>
2020-04-18 10:02:41 -0700

    Call directory changer service object

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-puts ENV['GITHUB_WORKSPACE']
+# Dir.children('/action/rails-minitest-action/services/initializers').each do |f|
+#   puts f
+# end
+
+require_relative './services/initializers/directory_changer'
+require_relative './services/initializers/dependency_installer'
+
+Initializers::DirectoryChanger.call(ENV['GITHUB_WORKSPACE'])
+# DependencyInstaller.call
