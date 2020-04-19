60a5f72fb003dbdccbdff249d8729b4133ebd9ea
Syntaf <gmercer015@gmail.com>
2020-04-18 17:49:57 -0700

    Install rails deps, stream output

--- src/main.rb
+++ src/main.rb
@@ -5,12 +5,15 @@
 # end
 
 require_relative './services/initializers/move_to_workspace'
-require_relative './services/initializers/dependency_installer'
+require_relative './services/initializers/rails_dependency_installer'
 
 require_relative './exceptions/missing_configuration'
 
+STDOUT.sync = true
+
 begin
   Initializers::MoveToWorkspace.call(:GITHUB_WORKSPACE)
+  Initializers::RailsDependencyInstaller.call
 rescue MissingConfiguration => e
   puts "Missing configuration: #{e.missing_field}"
 end
---
+++ src/services/initializers/rails_dependency_installer.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'pty'
+require_relative './../service'
+
+module Initializers
+  class RailsDependencyInstaller < Service
+    def call
+      `bundle config path vendor/bundle && \
+       bundle config --local build.sassc --disable-march-tune-native`
+
+      PTY.spawn('bundle install --jobs 4 --retry 3') do |stdout, _stdin, _pid|
+        puts 'Installing dependencies...'
+        stdout.each { |line| puts line }
+      end
+    rescue PTY::ChildExited, Errno::EIO
+      puts 'Installation complete'
+    end
+  end
+end
