69095fed32eb059b6c6ccf73965b5339747f6a03
Syntaf <gmercer015@gmail.com>
2020-04-24 09:38:20 -0700

    Properly rescue and return exit code of shell commands

--- src/tasks/task.rb
+++ src/tasks/task.rb
@@ -10,14 +10,16 @@ class Task
   end
 
   def shell(cmd)
-    PTY.spawn(cmd) do |stdout, _stdin, pid|
-      stdout.each { |line| puts line }
+    begin
+      PTY.spawn(cmd) do |stdout, _stdin, pid|
+        stdout.each { |line| puts line }
 
-      Process.wait(pid)
+        Process.wait(pid)
+      end
+    rescue PTY::ChildExited, Errno::EIO
+      # no-op for non-fatal exceptions
     end
-  rescue PTY::ChildExited, Errno::EIO
-    # non-fatal exit exceptions, no-op
-  ensure
+
     $CHILD_STATUS.exitstatus
   end
 end
